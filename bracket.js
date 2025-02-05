9fc47800d05a4476234fd85358f43742030d8a67
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-02-05 09:08:34 +0900

    프로그래머스-80 : 예상 대진표

---
+++ bracket.js
@@ -0,0 +1,15 @@
+function solution(n,a,b){
+   let answer = 1;
+   while(a!==b){
+      a = Math.round(a/2);
+      b = Math.round(b/2);
+      answer++;
+      if(a===b){
+         answer--;
+      }
+   }
+   
+   return answer;
+}
+
+console.log(solution(8,4,7))
